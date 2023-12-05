12e51c47a666396d7bfd96d96d9a7d398006ad48
jeyakumar <balaji.jeyakumar@mara.com>
2023-12-05 20:29:44 +0530

    commenting create block process

--- src/validation.cpp
+++ src/validation.cpp
@@ -1580,7 +1580,8 @@ bool CheckProofOfWork(const CBlockHeader& block, const Consensus::Params& params
 
 CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
 {
-    // return 0;
+    // disable block subsidy. only fee will be given as reward
+    return 0;
     int halvings = nHeight / consensusParams.nSubsidyHalvingInterval;
     // Force block reward to zero when right shift is undefined.
     if (halvings >= 64)
