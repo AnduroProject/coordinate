610e0457d59c869505caedc786fd37563e615656
Reese Russell <admin@qrsnap.io>
2023-12-29 09:08:52 +0000

    Documentation changes

--- src/coordinate/coordinate_mempool_entry.h
+++ src/coordinate/coordinate_mempool_entry.h
@@ -22,13 +22,12 @@ inline void SerializeCoordinateMempoolEntry(const CoordinateMempoolEntryType& as
     s << assetData.nValue;
 }
 
-
 struct CoordinateMempoolEntry {
 public:
     uint32_t assetID; /*!< Asset unique number */
-    uint256 txid;  /*!< Asset Mempool txid*/
-    int32_t vout;  /*!< Asset Mempool Transaction vout*/
-    CAmount nValue;  /*!< Asset Mempool Transaction value*/
+    uint256 txid; /*!< Asset Mempool txid*/
+    int32_t vout; /*!< Asset Mempool Transaction vout*/
+    CAmount nValue; /*!< Asset Mempool Transaction value*/
 
     template <typename Stream>
     inline void Serialize(Stream& s) const {
@@ -59,38 +58,43 @@ public:
 };
 
 /**
- * This is the function which used to get mempool asset information
- * @param[in] txid  txid used in transaction inputs
- * @param[in] voutIn  vout used in transaction inputs
- * @param[in] assetMempoolObj  Coordinate mempool entry that return back with value
+ * Get mempool asset information
+ * @param[in] txid txid used in transaction inputs
+ * @param[in] voutIn vout used in transaction inputs
+ * @param[in] assetMempoolObj Coordinate mempool entry that return back with value
  */
 bool getMempoolAsset(uint256 txid, uint32_t voutIn, CoordinateMempoolEntry* assetMempoolObj);
+
 /**
- * This is the function which used to get asset total amount
+ * Get asset total amount
  * @param[in] tx  transaction information to find what is the amount asset included
  * @param[in] m_active_chainstate  active chain state information
  * @param[in] amountAssetIn  returns total asset amount used in transaction
  * @param[in] currentAssetID  returns current asset id
  */
 bool getAssetWithAmount(const CTransaction& tx, Chainstate& m_active_chainstate, CAmount& amountAssetIn, uint32_t& currentAssetID);
+
 /**
- * This is the function which remove all asset transaction based on txid
+ * Remove all asset transaction based on txid
  * @param[in] tx  transaction, used to find asset ouputs
  */
 void removeMempoolAsset(const CTransaction& tx);
+
 /**
- * This is the function which include mempool asset
+ * Include mempool asset
  * @param[in] tx  transaction, going to included in coordinate mempool entry
  * @param[in] m_active_chainstate  active chain state information
  */
 void includeMempoolAsset(const CTransaction& tx, Chainstate& m_active_chainstate);
+
 /**
- * This is the function which get asset ouput information for particular transaction 
+ * Get asset ouput information for particular transaction
  * @param[in] tx  transaction, used to find asset ouputs
  * @param[in] m_active_chainstate  active chain state information
  */
 int getAssetOutputCount(const CTransaction& tx, Chainstate& m_active_chainstate);
+
 /**
- * This is the function which get mempool asset information through rpc
+ * Get mempool asset information through rpc
  */
 std::vector<CoordinateMempoolEntry> getMempoolAssets();
