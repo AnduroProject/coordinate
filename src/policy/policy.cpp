39aa33f425847a8537e01850ad2abd57fa303779
jeyakumar <balaji.jeyakumar@mara.com>
2024-05-14 18:28:42 +0530

    updating changes to live

--- src/policy/policy.cpp
+++ src/policy/policy.cpp
@@ -170,6 +170,10 @@ bool IsStandardTx(const CTransaction& tx, const std::optional<unsigned>& max_dat
 }
 
 bool AreCoordinateTransactionStandard(const CTransaction& tx, CCoinsViewCache& mapInputs) {
+    if(tx.nVersion != TRANSACTION_COORDINATE_ASSET_TRANSFER_VERSION || tx.nVersion != TRANSACTION_COORDINATE_ASSET_CREATE_VERSION) {
+       return true;
+    }
+
     CAmount amountAssetInOut = CAmount(0); 
     uint32_t currentAssetID = 0;
     for (unsigned int i = 0; i < tx.vin.size(); i++) {
@@ -189,10 +193,7 @@ bool AreCoordinateTransactionStandard(const CTransaction& tx, CCoinsViewCache& m
         } else {
             // check input is unspent
             bool is_asset = mapInputs.getAssetCoin(tx.vin[i].prevout,fBitAsset,fBitAssetControl,nAssetID, &coin);
-            if(!is_asset) {
-                LogPrintf("Invalid inputs \n");
-                return false;
-            } else {
+            if(is_asset) {
                 coinValue = coin.out.nValue;
             }
         }
