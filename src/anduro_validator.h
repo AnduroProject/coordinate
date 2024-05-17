a9c3b2dd994b82ef1fb9721b6a60a8b503aa617f
Portland.HODL <admin@qrsnap.io>
2024-05-17 01:36:34 -0700

    Update anduro_validator.h - Docs

--- src/anduro_validator.h
+++ src/anduro_validator.h
@@ -5,30 +5,30 @@
 #include <key_io.h>
 
 /**
- * This function check witness signature path available in authorized anduro keys
- * @param[in] fullQuorum  authorized anduro full path
- * @param[in] signaturePath  path signed by the anduro for presigned block
+ * Check the witness signature path available in authorized Anduro keys
+ * @param[in] fullQuorum authorized Anduro full path
+ * @param[in] signaturePath path signed by the Anduro for presigned block
  */
 bool getRedeemPathAvailable(std::vector<std::string> fullQuorum, std::string signaturePath);
 
 /**
- * This function check used to prepare sha256 hash for presigned block message
- * @param[in] message  presigned block message
+ * Check used to prepare sha256 hash for presigned block message
+ * @param[in] message presigned block message
  */
 uint256 prepareMessageHash(std::string message);
-
+[
 /**
- * This function used to validate presigned signature
- * @param[in] witnessHex  block witness which hold signature path and signature
- * @param[in] message  sha256 presigned block message
- * @param[in] prevWitnessHex anduro current keys
+ * Validate presigned signature
+ * @param[in] witnessHex block witness which hold signature path and signature
+ * @param[in] message sha256 presigned block message
+ * @param[in] prevWitnessHex Anduro current keys
 */
 bool validateAnduroSignature(std::string witnessHex, std::string message, std::string prevWitnessHex);
 
 /**
  * This function used to validate presigned signature
- * @param[in] signatureHex  block witness which hold signature path and signature
- * @param[in] messageIn  sha256 presigned block message
- * @param[in] prevWitnessHex anduro current keys
+ * @param[in] signatureHex block witness which hold signature path and signature
+ * @param[in] messageIn sha256 presigned block message
+ * @param[in] prevWitnessHex Anduro current keys
 */
 bool validatePreconfSignature(std::string signatureHex, std::string messageIn, std::string prevWitnessHex);
